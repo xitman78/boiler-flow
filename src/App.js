27ebe082fa517c8e7dc0b316c862a1b54fe590c9
Alexander Cherepnya <xitman78@me.com>
2017-12-16 22:26:39 +0200

    added .env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+// @flow
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+type Props = {
+  bar?: string,
+};
+
+class App extends Component<Props> {
+
+ /* test(a: string) {
+
+  }*/
+
   render() {
+
+   // this.test(55);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {`${process.env.TEST_ENV}`}
       </div>
     );
   }
