d158bd010e847f49da917a866caebf6acceca247
Alexander Cherepnya <xitman78@me.com>
2017-12-17 21:34:52 +0200

    input component

---
+++ src/componets/form/input.js
@@ -0,0 +1,30 @@
+// @flow
+
+import React, {PureComponent} from 'react';
+
+type Props = {
+  label: string,
+  value: string,
+  property: string,
+  onChange: Function,
+};
+
+
+class FormInput extends PureComponent<Props> {
+
+  onChange = (event: SyntheticInputEvent<>, value: string) => {
+    this.props.onChange(event.target.value, this.props.property);
+  };
+
+  render() {
+
+    return <div>
+      {this.props.label}
+      <input type="text" value={this.props.value} onChange={this.onChange} />
+    </div>;
+
+  }
+
+}
+
+export default FormInput;
--- src/componets/users/edit.js
+++ src/componets/users/edit.js
@@ -3,6 +3,8 @@ import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {getUser} from "./actions";
 
+import FormInput from '../form/input';
+
 import type {ActionType} from "../../actions/actionTypes";
 import type {UsersState, User} from "../../store/storeTypes";
 
@@ -13,17 +15,37 @@ type Props = {
   getUser: () => ActionType,
 };
 
-class UserEdit extends Component<Props> {
+class UserEdit extends Component<Props, {}> {
 
   componentWillMount() {
+
+    this.setState({});
+
     this.userId = this.props.match.params.id;
     this.props.getUser(this.userId);
   }
 
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.user) {
+      this.setState({user: Object.assign({}, nextProps.user)});
+    }
+  }
+
+  onInputChange = (value: string, property: string) => {
+    let user = Object.assign({}, this.state.user);
+    user[property] = value;
+    this.setState({user: user});
+  };
+
   render() {
 
     return <div className="users-list-container">
-      {this.props.user && this.props.user.firstName}
+      {this.state.user && <div>
+          <FormInput label="First name:" value={this.state.user.firstName} property="firstName" onChange={this.onInputChange} />
+          <FormInput label="Last name:" value={this.state.user.lastName} property="lastName" onChange={this.onInputChange} />
+          <FormInput label="E-mail:" value={this.state.user.email} property="email" onChange={this.onInputChange} />
+        </div>
+      }
       </div>;
   }
 
