e1c4c95d5748b5f8ed7fbf918b9da9190b5181f2
Alexander Cherepnya <xitman78@me.com>
2018-01-05 00:33:17 +0200

    Some theming

--- src/styles/createContext.js
+++ src/styles/createContext.js
@@ -3,14 +3,30 @@
 import { create, SheetsRegistry } from 'jss';
 import preset from 'jss-preset-default';
 import { createMuiTheme } from 'material-ui/styles';
-import { blueGrey, blue } from 'material-ui/colors';
+import { blueGrey, indigo } from 'material-ui/colors';
 import createGenerateClassName from 'material-ui/styles/createGenerateClassName';
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
-    secondary: blueGrey,
+    // type: 'dark',
+    primary: blueGrey,
+    secondary: indigo,
   },
+  /*overrides: {
+    MuiButton: {
+      // Name of the styleSheet
+      root: {
+        // Name of the rule
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+      },
+    },
+  },*/
 });
 
 // Configure JSS
